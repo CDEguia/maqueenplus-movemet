875d2209db0086d018573b1fa272f816b7f5d264
CDEguia <candelario.eguia@laverne.edu>
2021-09-02 19:16:35 -0700

    Begin masive overhaul

--- main.py
+++ main.py
@@ -1,43 +1,71 @@
+LEFT = 0
+RIGHT = 1
+FORWARD = 2
+BACKWARD = 3
+DAMAGE = 4
+
+convertedString = []
+
+basespeed = 0
+quarterTurn = 0
+turnSpeed = 0
+fullmove = 0
+smallmove = 0
+radio.set_group(1)
+
+basespeed = 50
+turnSpeed = 50
+
+quarterTurn = 0.35
+fullmove = 0.75
+baseMove = 0.01
+moveAmount = 0
+
+def calculate_turn(receivedValue: number, type: number):
+    
+    moveAmount = (receivedValue*35)*baseMove
+
+def calculate_squars(receicedValue: number):
+    mo
+
+
+def convert_received_string(received: str):
+    for i in 2:
+        convertedString.append(int(received[i]))
 
 def on_received_string(receivedString):
-    if receivedString == "oneForward":
-        display_direction("forward")
-        MoveForward(fullmove)
-    elif receivedString == "oneBackward":
-        display_direction("backward")
-        MoveBackward(fullmove)
-    elif receivedString == "oneLeft":
-        display_direction("left")
+    convert_received_string(receivedString)
+
+    display_direction(convertedString[0])
+    
+    if convertedString[0] >= RIGHT :
+        calculate_turn(convertedString[1])
+    elif convertedString[1] >= BACKWARD :
+        cal
+    if convertedString[0] >= BACKWARD :
+        MoveBackward(convertedString[1])
+    elif convertedString[0] == FORWARD:
+        MoveForward(convertedString[1])
+    elif convertedString[0] == LEFT:
         turn_signal("left", 4)
-        TurnLeft(quarterTurn)
-    elif receivedString == "oneRight":
-        display_direction("right")
+        TurnLeft(convertedString[1])
+    elif convertedString[0] == RIGHT:
         turn_signal("right", 4)
-        TurnRight(quarterTurn)
-    elif receivedString == "littleForward":
-        display_direction("forward")
-        MoveForward(smallmove)
-    elif receivedString == "littleBackward":
-        display_direction("backward")
-        MoveBackward(smallmove)
-    elif receivedString == "littleLeft":
-        display_direction("left")
-        turn_signal("left", 2)
-        TurnLeft(smallmove)
-    elif receivedString == "littleRight":
-        display_direction("right")
-        turn_signal("right", 2)
-        TurnRight(smallmove)
+        TurnRight(convertedString[1])
+#    elif receivedString == "littleForward":
+#        MoveForward(smallmove)
+#    elif receivedString == "littleBackward":
+#        MoveBackward(smallmove)
+#    elif receivedString == "littleLeft":
+#        turn_signal("left", 2)
+#        TurnLeft(smallmove)
+#    elif receivedString == "littleRight":
+#        turn_signal("right", 2)
+#        TurnRight(smallmove)
     else:
         pass
     DFRobotMaqueenPlus.motot_stop(Motors.ALL)
-    basic.show_leds("""
-        # . . . #
-        . # # # .
-        . # # . .
-        . # . . .
-        . . . . .
-    """)
+
     DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.WHITH)
     DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.WHITH)
 radio.on_received_string(on_received_string)
@@ -107,7 +135,7 @@ def turn_signal(direction: str, blink: number):
 
 
 def display_direction(direction: str):
-    if direction == "forward":
+    if (direction == "oneForward") or (direction == "littleForward"):
         basic.show_leds("""
             . . # . .
             . # . # .
@@ -115,7 +143,7 @@ def display_direction(direction: str):
             . . # . .
             . . # . .
         """)
-    elif direction == "backward":
+    elif (direction == "oneBackward") or (direction == "littleBackward"):
         basic.show_leds("""
             . . # . .
             . . # . .
@@ -123,7 +151,7 @@ def display_direction(direction: str):
             . # . # .
             . . # . .
         """)
-    elif direction == "left":
+    elif (direction == "oneLeft") or (direction == "littleLeft"):
         basic.show_leds("""
             . . # . .
             . . . # .
@@ -131,7 +159,7 @@ def display_direction(direction: str):
             . . . # .
             . . # . .
         """)
-    else:
+    elif (direction == "oneLeft") or (direction == "littleRight"):
         basic.show_leds("""
             . . # . .
             . # . . .
@@ -139,20 +167,14 @@ def display_direction(direction: str):
             . # . . .
             . . # . .
         """)
-
-basespeed = 0
-quarterTurn = 0
-turnSpeed = 0
-fullmove = 0
-smallmove = 0
-radio.set_group(1)
-
-basespeed = 50
-turnSpeed = 50
-
-quarterTurn = 0.35
-fullmove = 0.75
-smallmove = 0.01
+    else:
+        basic.show_leds("""
+            # . . . #
+            . # # # .
+            . # # . .
+            . # . . .
+            . . . . .
+        """)
 
 def on_forever():
     pass
